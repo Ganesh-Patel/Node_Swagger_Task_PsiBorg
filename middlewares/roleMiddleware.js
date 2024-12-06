0f7943606b14ff8e996dc964eb9557132a1d541a
Ganesh-Patel-U <ganesh.oficial158@gmail.com>
2024-12-06 17:16:51 +0530

    role update

--- middlewares/roleMiddleware.js
+++ middlewares/roleMiddleware.js
@@ -1,7 +1,7 @@
 
 export const authorizeRoles = (...allowedRoles) => {
     return (req, res, next) => {
-      const userRole = req.user?.role[0];
+      const userRole = req.user?.roles[0];
   
       if (!userRole || !allowedRoles.includes(userRole)) {
         return res.status(403).json({ message: "Access denied" });
