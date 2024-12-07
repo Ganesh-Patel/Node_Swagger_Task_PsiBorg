716562d90d0fddf6ae89c5159e32d1b7ebfead73
Ganesh-Patel-U <ganesh.oficial158@gmail.com>
2024-12-07 15:58:54 +0530

    role middleware updated

--- middlewares/roleMiddleware.js
+++ middlewares/roleMiddleware.js
@@ -1,12 +1,21 @@
 
 export const authorizeRoles = (...allowedRoles) => {
-    return (req, res, next) => {
-      const userRole = req.user?.roles[0];
-  
-      if (!userRole || !allowedRoles.includes(userRole)) {
+  return (req, res, next) => {
+    try {
+      const userRole = req.user?.roles[0]?.toLowerCase();
+
+      if (
+        !userRole || 
+        !allowedRoles.map(role => role.toLowerCase()).includes(userRole)
+      ) {
         return res.status(403).json({ message: "Access denied" });
       }
+
       next();
-    };
+    } catch (error) {
+      return res.status(500).json({ message: "Error in role authorization", error: error.message });
+    }
   };
+};
+
   
